035826782e8d662b3c315346d722fbf9ca6dd122
Thomas Fournier <thomasfournierbrest@gmail.com>
2023-12-21 10:21:00 +0100

    First commit addition

---
+++ calculatrice.py
@@ -0,0 +1,10 @@
+#nombre_a = int(input("Entrez un nombre: "))
+#nombre_b = int(input("Entrez un autre nombre: "))
+#resultat = (nombre_a + nombre_b)
+#print(resultat)
+
+a = input("Entrez un nombre: ")
+b = input("Entrez un nombre: ")
+
+resultat = f"Le résultat de l'addition de {a} + {b} est égale à {int(a) + int(b)}"
+print(resultat)
