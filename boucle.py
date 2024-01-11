cdbe7071d5130a9d2632c04f9cd7db5920d8f4f5
Thomas Fournier <thomasfournierbrest@gmail.com>
2024-01-11 10:12:43 +0100

    add liste de course

--- boucle.py
+++ boucle.py
@@ -56,3 +56,6 @@ nombres_inverses = [i if i % 2 == 0 else -i for i in nombres]
 
 print(nombres_inverses)
 """
+
+#Modification
+
--- calculatrice.py
+++ calculatrice.py
@@ -1,10 +1,24 @@
+"""
 #nombre_a = int(input("Entrez un nombre: "))
 #nombre_b = int(input("Entrez un autre nombre: "))
 #resultat = (nombre_a + nombre_b)
 #print(resultat)
 
-a = input("Entrez un nombre: ")
-b = input("Entrez un nombre: ")
+#On décalre deux variables
+a = b = ""
 
+#On boucle tant a et b ne sont pas des entiers
+while not(a.isdigit() and b.isdigit()):
+
+#On demande avec input un nombre à l'utilisateur
+    a = input("Entrez un nombre: ")
+    b = input("Entrez un nombre: ")
+
+#Conditions, on affiche une phrase si l'utilisateur n'entre pas des lettres
+    if not(a.isdigit() and b.isdigit()):
+        print("Veuillez entrer des nombres valides")
+
+#On affiche le résultat de l'addition
 resultat = f"Le résultat de l'addition de {a} + {b} est égale à {int(a) + int(b)}"
 print(resultat)
+"""
